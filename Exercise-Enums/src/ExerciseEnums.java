a219132ec5da8b6b6cc1744eea691f2242e4e1fd
johnathon-stone <86032402+johnathon-stone@users.noreply.github.com>
2025-05-29 12:28:37 -0500

    Update ExerciseEnums.java
    Completed part 1 per documentation.

--- Exercise-Enums/src/ExerciseEnums.java
+++ Exercise-Enums/src/ExerciseEnums.java
@@ -1,5 +1,18 @@
 public class ExerciseEnums {
+
+//  Part 1. Define and Use an Enum
+//   1. Create an enum named 'CoffeeSize' with three constants.
+    enum CoffeeSize {
+        SMALL,
+        MEDIUM,
+        LARGE
+}
     public static void main(String[] args) {
 
+//       2. Declare and initialize a variable of type 'CoffeeSize' to store a predefined coffee order.
+        CoffeeSize coffeeOrder = CoffeeSize.MEDIUM;
+
+//       3. Print the selected coffee size.
+        System.out.println("Selected coffee size: " + coffeeOrder);
     }
 }
