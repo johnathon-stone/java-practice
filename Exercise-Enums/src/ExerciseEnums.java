914d3c46745636054f376462c354c8a1acdeb58a
johnathon-stone <86032402+johnathon-stone@users.noreply.github.com>
2025-05-29 12:46:43 -0500

    Update ExerciseEnums.java
    Completed part 2 per documentation and formatted comments and output to make it legible.

--- Exercise-Enums/src/ExerciseEnums.java
+++ Exercise-Enums/src/ExerciseEnums.java
@@ -1,18 +1,36 @@
 public class ExerciseEnums {
 
 //  Part 1. Define and Use an Enum
-//   1. Create an enum named 'CoffeeSize' with three constants.
+//   p1.1. Create an enum named 'CoffeeSize' with three constants.
     enum CoffeeSize {
         SMALL,
         MEDIUM,
         LARGE
 }
+
+//  Part 2. Convert Enums to integer Values
+//   p2.1. Create an enum named SeatSection with values.
+    enum SeatSection {
+        GENERAL,
+        PREMIUM,
+        VIP
+}
+
     public static void main(String[] args) {
 
-//       2. Declare and initialize a variable of type 'CoffeeSize' to store a predefined coffee order.
+//       p1.2. Declare and initialize a variable of type 'CoffeeSize' to store a predefined coffee order.
         CoffeeSize coffeeOrder = CoffeeSize.MEDIUM;
 
-//       3. Print the selected coffee size.
-        System.out.println("Selected coffee size: " + coffeeOrder);
+//       p1.3. Print the selected coffee size.
+        System.out.printf("Selected coffee size: %s\n\n", coffeeOrder);
+
+//       p2.2. Print each section's corresponding integer value using the 'ordinal()' method.
+        SeatSection sectionG = SeatSection.GENERAL;
+        SeatSection sectionP = SeatSection.PREMIUM;
+        SeatSection sectionV = SeatSection.VIP;
+
+        System.out.println(sectionG + " is assigned value: " + sectionG.ordinal());
+        System.out.println(sectionP + " is assigned value: " + sectionP.ordinal());
+        System.out.println(sectionV + " is assigned value: " + sectionV.ordinal());
     }
 }
