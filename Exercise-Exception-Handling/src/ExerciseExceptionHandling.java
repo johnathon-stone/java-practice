0cd398ab089812971935da9ea8147ff8a0e7dbca
johnathon-stone <86032402+johnathon-stone@users.noreply.github.com>
2025-06-05 10:51:21 -0500

    Update ExerciseExceptionHandling.java
    Updated ExerciseExceptionHandling.java completing all task required. Ready for review.

--- Exercise-Exception-Handling/src/ExerciseExceptionHandling.java
+++ Exercise-Exception-Handling/src/ExerciseExceptionHandling.java
@@ -1,5 +1,64 @@
+import java.util.Scanner;
+
 public class ExerciseExceptionHandling {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        /*Placeholder for user input.*/
+        int age;
+        String email;
+        int pin;
+
+        /*Age validation loop.*/
+        while (true) {
+            System.out.print("Enter your age: ");
+            try {
+                age = Integer.parseInt(scanner.nextLine());
+                if (age > 0 && age < 115) {
+                    break;
+                } else {
+                    System.out.println("Invalid age entered. Please try again.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Error: " + e.getMessage() + " Try again.");
+            }
+        }
+
+        /*Email validation loop.*/
+        while (true) {
+            System.out.print("Enter your email: ");
+            email = scanner.nextLine().trim();
+            if (email.isEmpty()) {
+                System.out.println("Error: Email can't be null.");
+            } else {
+                break;
+            }
+        }
+
+        while (true) {
+            System.out.print("Enter your 4-digit PIN: ");
+
+            try {
+                pin = Integer.parseInt(scanner.nextLine());
+                String pinSTR = String.valueOf(pin);
+                if (pinSTR.length() == 4) {
+                    break;
+                } else {
+                    System.out.println("Error: Not a 4 digit integer.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Error: " + e.getMessage() + " Try again.");
+            } finally {
+                System.out.println("Registration Attempt Complete!");
+            }
+        }
+
+
+        System.out.println("\nRegistration Successful!");
+        System.out.println("Age: " + age);
+        System.out.println("Email: " + email);
+        System.out.println("PIN: " + pin);
 
     }
 }
