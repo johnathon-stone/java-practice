eef76c33061b7d2d08e8572d4099d81da25eb141
johnathon-stone <86032402+johnathon-stone@users.noreply.github.com>
2025-06-06 14:08:02 -0500

    Update ShoppingCartApp.java
    Updated ShoppingCartApp.java according to documentation. Ready for review.

--- Exercise-Shopping-Cart/src/ShoppingCartApp.java
+++ Exercise-Shopping-Cart/src/ShoppingCartApp.java
@@ -1,50 +1,98 @@
 import java.util.Scanner;
 
 public class ShoppingCartApp {
+
+    /*Method used to print an array.*/
+    public static void displayChoices(String[] choices) {
+        for (int i = 0; i < choices.length; i++) {
+            System.out.println((i + 1) + ". " + choices[i]);
+        }
+    }
+
+    /*Method used to print prompt, gather, validate, and return integer input from user.*/
+    private static int promptUserForInt(String prompt) {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        boolean isValid = false;
+        int result = -1;
+        while (!isValid) {
+            System.out.println(prompt);
+            try {
+                result = Integer.parseInt(input.nextLine());
+                isValid = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number.");
+            }
+        }
+        return result;
+    }
+
+    /*Method used to print prompt, gather, and return string input from user.*/
+    private static String promptUserForString(String prompt) {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        System.out.println(prompt);
+        return input.nextLine();
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         /* Declare and assign variables. */
-        String businessName = "Wally World";
-        String contactInfo = "789-240-6384";
-        int prodID = 1;
-        String prodDesc = "The product description";
-        int prodCategory = 2;
-        double prodCost = 2.56;
-        double prodPrice = 4.99;
-        int prodQty = 78;
+        boolean isConfirmed = false;
         String taxExempt = null;
         String shipping = null;
-        String orderQty = null;
+        int orderQty = 0;
         String promoCode = null;
 
-        /* Get input from user to assign to variables. */
-        System.out.println("Are you tax-exempt? (y/n)");
-        taxExempt = input.nextLine();
-        System.out.println("Shipping? (y/n)");
-        shipping = input.nextLine();
-        System.out.println("Order quantity? ");
-        orderQty = input.nextLine();
-        System.out.println("Promo code for free shipping? ");
-        promoCode = input.nextLine();
+        /*Enum assigned values.*/
+        enum OrderStatus {
+            PENDING, PROCESSING, SHIPPED, DELIVERED
+        }
 
-        /* Code to calculate the total cost of the product based on the inventory & print the value. */
-        double totalCost = prodCost * prodQty;
-        System.out.printf("The total cost for the full inventory of product ID: %d is: $%.2f", prodID, totalCost);
+        enum ShippingStatus {
+            STANDARD, TWO_DAY, OVERNIGHT
+        }
 
-        /* Code to calculate the profit margin of the product & print the value. */
-        double perItemProfit = prodPrice - prodCost;
-        System.out.printf("%nOur expected profit margin for product ID: %d is: $%.2f", prodID, perItemProfit);
+        String[] addresses = {"123 Main St", "456 1st Street", "789 23rd Street"};
+        String[] sizes = {"small", "medium", "large"};
 
-        /* Code to calculate the total potential profit & print the value. */
-        double potentialProfit = perItemProfit * prodQty;
-        System.out.printf("%nOur expected profit margin for product ID: %d is: $%.2f", prodID, potentialProfit);
+        /* While loops to continue to get valid input from user to assign to variables. */
+        while (!isConfirmed) {
+            String address = null;
+            String size = null;
+            taxExempt = promptUserForString("Are you tax-exempt? (y/n)");
+            while (address == null) {
+                try {
+                    displayChoices(addresses);
+                    int addressIndex = promptUserForInt("Shipping address?") - 1;
+                    address = addresses[addressIndex];
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Please enter a valid menu option.");
+                }
+            }
 
-        /* Code printing the string variables. */
-        System.out.println("Business name: " + businessName);
-        System.out.println("Business contact info: " + contactInfo);
-        System.out.println("Product description: " + prodDesc);
+            shipping = promptUserForString("Shipping? (standard/overnight/twoday)");
+            while (size == null) {
+                try {
+                    displayChoices(sizes);
+                    int sizeIndex = promptUserForInt("Sizes?") - 1;
+                    size = sizes[sizeIndex];
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Please enter a valid menu option.");
+                }
+            }
 
-        System.out.println("\nTax-exempt: " + taxExempt + "\nShipping: " + shipping + "\nOrder quantity: " + orderQty + "\nPromo code: " + promoCode);
+            orderQty = promptUserForInt("Order quantity?");
+            promoCode = promptUserForString("Promo code for free shipping? ");
+            System.out.println("\n----Details----");
+            System.out.println("Tax-exempt: " + taxExempt);
+            System.out.println("Address: " + address);
+            System.out.println("Shipping: " + shipping);
+            System.out.println("Size: " + size);
+            System.out.println("Order quantity: " + orderQty);
+            System.out.println("Promo code: " + promoCode);
+            System.out.println("Confirm order y/n: ");
+            isConfirmed = "y".equals(input.nextLine());
+        }
+        System.out.println("\nGoodbye.");
     }
 }
