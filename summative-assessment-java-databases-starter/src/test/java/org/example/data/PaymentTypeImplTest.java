5e339eea241db41898da47ea2dab53f959fd32ed
johnathon-stone <86032402+johnathon-stone@users.noreply.github.com>
2025-08-01 12:14:23 -0500

    Update PaymentTypeImplTest.java
    Updated PaymentTypeImplTest.java

--- summative-assessment-java-databases-starter/src/test/java/org/example/data/PaymentTypeImplTest.java
+++ summative-assessment-java-databases-starter/src/test/java/org/example/data/PaymentTypeImplTest.java
@@ -1,9 +1,15 @@
 package org.example.data;
 
+import org.example.data.exceptions.InternalErrorException;
+import org.example.model.PaymentType;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 @SpringBootTest
 public class PaymentTypeImplTest {
@@ -17,5 +23,13 @@ public class PaymentTypeImplTest {
     @BeforeEach
     void setUp() {
         jdbcTemplate.execute("{call set_known_good_state()}");
+    }
+
+    @Test
+    void getAll_ReturnsList() throws InternalErrorException {
+        List<PaymentType> testList = paymentTypeRepo.getAll();
+
+        assertNotNull(testList);
+        assertEquals(5, testList.size());
     }
 }
