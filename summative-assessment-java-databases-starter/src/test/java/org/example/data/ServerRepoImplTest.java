f93196253c68bf55d2b0ec02fb229b3383600870
johnathon-stone <86032402+johnathon-stone@users.noreply.github.com>
2025-08-01 12:06:06 -0500

    Update ServerRepoImplTest.java
    Updated ServerRepoImplTest.java with test using both valid and invalid variables. All tests pass.

--- summative-assessment-java-databases-starter/src/test/java/org/example/data/ServerRepoImplTest.java
+++ summative-assessment-java-databases-starter/src/test/java/org/example/data/ServerRepoImplTest.java
@@ -1,9 +1,18 @@
 package org.example.data;
 
+import org.example.data.exceptions.InternalErrorException;
+import org.example.data.exceptions.RecordNotFoundException;
+import org.example.model.Server;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class ServerRepoImplTest {
@@ -17,5 +26,43 @@ public class ServerRepoImplTest {
     @BeforeEach
     void setUp() {
         jdbcTemplate.execute("{call set_known_good_state()}");
+    }
+
+    @Test
+    void getServerById_ValidData_ReturnsServer() throws RecordNotFoundException, InternalErrorException {
+        int testId = 1;
+
+        Server server = serverRepo.getServerById(testId);
+
+        assertNotNull(server);
+        assertEquals("Mersey", server.getFirstName());
+        assertEquals("Giacometti", server.getLastName());
+    }
+
+    @Test
+    void getServerById_InvalidData_ThrowsException() throws RecordNotFoundException, InternalErrorException {
+        int testId = 99;
+
+        assertThrows(InternalErrorException.class, () -> serverRepo.getServerById(testId));
+    }
+
+    @Test
+    void getAllAvailableServers_ValidData_ReturnsServers() throws RecordNotFoundException, InternalErrorException {
+        LocalDate testDate = LocalDate.now();
+
+        List<Server> servers = serverRepo.getAllAvailableServers(testDate);
+
+        assertNotNull(servers);
+        assertFalse(servers.isEmpty());
+        assertEquals(14, servers.size());
+    }
+
+    @Test
+    void getAllAvailableServers_InvalidData_ReturnsEmpty() throws RecordNotFoundException, InternalErrorException {
+        LocalDate testDate = LocalDate.now().minusYears(20);
+
+        List<Server> servers = serverRepo.getAllAvailableServers(testDate);
+
+        assertEquals(0, servers.size());
     }
 }
