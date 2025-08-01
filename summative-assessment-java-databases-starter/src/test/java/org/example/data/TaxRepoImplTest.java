6cb314a8ca427aa55f6a5181a1434f3b3b74dc02
johnathon-stone <86032402+johnathon-stone@users.noreply.github.com>
2025-08-01 11:53:33 -0500

    Update TaxRepoImplTest.java
    Updated TaxRepoImplTest.java with valid and invalid results. All tests pass.

--- summative-assessment-java-databases-starter/src/test/java/org/example/data/TaxRepoImplTest.java
+++ summative-assessment-java-databases-starter/src/test/java/org/example/data/TaxRepoImplTest.java
@@ -1,9 +1,17 @@
 package org.example.data;
 
+import org.example.data.exceptions.InternalErrorException;
+import org.example.data.exceptions.RecordNotFoundException;
+import org.example.model.Tax;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class TaxRepoImplTest {
@@ -19,5 +27,20 @@ public class TaxRepoImplTest {
         jdbcTemplate.execute("{call set_known_good_state()}");
     }
 
+    @Test
+    void getCurrentTax_ValidData_ReturnsCurrentTax() throws RecordNotFoundException, InternalErrorException {
+        LocalDate testDate = LocalDate.now();
 
+        Tax tax = taxRepo.getCurrentTax(testDate);
+
+        assertNotNull(tax);
+        assertEquals(6.25, tax.getTaxPercentage().doubleValue());
+    }
+
+    @Test
+    void getCurrentTax_InvalidData_ReturnsNull() throws RecordNotFoundException, InternalErrorException {
+        LocalDate testDate = LocalDate.now().minusYears(20);
+
+        assertThrows(InternalErrorException.class, () -> taxRepo.getCurrentTax(testDate));
+    }
 }
